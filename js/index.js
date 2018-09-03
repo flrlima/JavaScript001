0ba0006eb840f0663deec947c6a60e70468383c4
Fabio Lima <fabio.lima@vvssistemas.local>
2018-09-03 14:32:42 -0300

    Testando a prioridade da extensão

--- js/index.js
+++ js/index.js
@@ -8,13 +8,30 @@ $(document).ready(function(){
         var nome = $("#idNome").val();
         var email = $("#idEmail").val();
         var mensagemErro = "";
+        
         if(nome == "")
+        {
             mensagemErro += "Campo nome é obrigatório\n";
+            console.log(mensagemErro += "Campo nome é obrigatório\n");
+        }
+            
         if(email == "")
+        {
             mensagemErro += "Campo e-mail é obrigadtório\n";
+            console.log(mensagemErro += "Campo e-mail é obrigadtório\n");
+        }
+            
         if(mensagemErro == "")
+        {
             alert($("#idNome").val() + ", você foi cadastrado com sucesso!");
+            console.log($("#idNome").val() + ", você foi cadastrado com sucesso!");
+        }
+            
         else
+        {
+            console.log(mensagemErro);
             return alert(mensagemErro);
+        }
+
     }
 });
